405e6c37c20aa3311f6fcf211e864d7f583a16da
Irene <owlishDeveloper@users.noreply.github.com>
2017-04-08 16:47:57 -0700

    Update selectionSort.py
    Changed names to Python style

--- sort/selectionSort.py
+++ sort/selectionSort.py
@@ -1,29 +1,29 @@
 # O(n^2)
 
-def selectionSort (List):
+def selection_sort (l):
   '''
     Sorts a list in increasing order.
     First, selects the smallest number and swaps it with the first number.
     Then selects the second smallest number and swaps it with the second number.
     And so on.
   '''
-  for i in range( len(List) - 1 ):
-    nextSmallest_i = getNextSmallest(List, i)
-    (List[i], List[nextSmallest_i]) = (List[nextSmallest_i], List[i])
+  for i in range( len(l) - 1 ):
+    next_smallest_i = get_next_smallest(l, i)
+    (List[i], List[next_smallest_i]) = (List[next_smallest_i], List[i])
     
     
-def getNextSmallest (List, startIndex):
+def get_next_smallest (l, start_index):
   '''
     Helper function to find the index of the smallest number
     in the portion of the list after the start index.
     Returns the index of the number.
   '''
-  minValue = List[startIndex]; 
-  index = startIndex;
+  min_value = l[start_index]; 
+  index = start_index;
     
-  for i in range( startIndex + 1, len(List) ):
-    if List[i] < minValue:
-      minValue = List[i]
+  for i in range( start_index + 1, len(l) ):
+    if l[i] < min_value:
+      min_value = l[i]
       index = i
     
   return index
