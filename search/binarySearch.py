6236576361dc9ea47000bdfee2d84117b28b17a1
Irene <owlishDeveloper@users.noreply.github.com>
2017-04-08 16:45:17 -0700

    Update binarySearch.py

--- search/binarySearch.py
+++ search/binarySearch.py
@@ -1,27 +1,27 @@
 # O(lb(n))
 
 # While-loop version
-def biSearch (List, target):
+def bisection_search (l, target):
   '''
-    Searches a given number (target) in a SORTED list of numbers (List)
+    Searches a given number (target) in a SORTED list of numbers (l)
     Returns the index of that number in the list or -1 if not found
   '''
   min = 0
-  max = len(List) - 1
+  max = len(l) - 1
   
   while (max >= min):
     guess = int( (min + max) / 2 )
-    if List[guess] == target:
+    if l[guess] == target:
       return guess
-    elif List[guess] < target:
+    elif l[guess] < target:
       min = guess + 1;
-    elif List[guess] > target:
+    elif l[guess] > target:
       max = guess - 1;
       
   return -1
   
   # Recursive version
-  def biSearchR (List, target, min = None, max = None):
+  def bisection_search_r (l, target, min = None, max = None):
   '''
     Searches a given number (target) in a SORTED list of numbers (List)
     Returns the index of that number in the list or -1 if not found
@@ -29,18 +29,18 @@ def biSearch (List, target):
   if min is None:
     min = 0
   if max is None:
-    max = len(List) - 1
+    max = len(l) - 1
   
   if max < min:
     return -1
     
   guess = int( (min + max) / 2 )
   
-  if List[guess] == target:
+  if l[guess] == target:
     return guess
-  elif List[guess] < target:
+  elif l[guess] < target:
     min = guess + 1;
-    return biSearchR(List, target, min, max)
-  elif List[guess] > target:
+    return bisection_search_r(List, target, min, max)
+  elif l[guess] > target:
     max = guess - 1;
-    return biSearchR(List, target, min, max)
+    return bisection_search_r(l, target, min, max)
