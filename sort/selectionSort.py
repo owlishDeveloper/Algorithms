0e6cdab4dfb9f966e99c3668c7d200b0b76aa8a4
Irene <owlishDeveloper@users.noreply.github.com>
2017-04-10 10:13:27 -0700

    Update selectionSort.py

--- sort/selectionSort.py
+++ sort/selectionSort.py
@@ -1,29 +1,29 @@
 # O(n^2)
 
-def selection_sort (l):
+def selection_sort (a_list):
   '''
     Sorts a list in increasing order.
     First, selects the smallest number and swaps it with the first number.
     Then selects the second smallest number and swaps it with the second number.
     And so on.
   '''
-  for i in range( len(l) - 1 ):
-    next_smallest_i = get_next_smallest(l, i)
-    (List[i], List[next_smallest_i]) = (List[next_smallest_i], List[i])
+  for i in range( len(a_list) - 1 ):
+    next_smallest_i = get_next_smallest(a_list, i)
+    ( a_list[i], a_list[next_smallest_i] ) = ( a_list[next_smallest_i], a_list[i] )
     
     
-def get_next_smallest (l, start_index):
+def get_next_smallest (a_list, start_index):
   '''
     Helper function to find the index of the smallest number
     in the portion of the list after the start index.
     Returns the index of the number.
   '''
-  min_value = l[start_index]; 
+  min_value = a_list[start_index]; 
   index = start_index;
     
-  for i in range( start_index + 1, len(l) ):
-    if l[i] < min_value:
-      min_value = l[i]
+  for i in range( start_index + 1, len(a_list) ):
+    if a_list[i] < min_value:
+      min_value = a_list[i]
       index = i
     
   return index
