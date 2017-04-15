d4d3dff088568e42c1a52e058aeed24751ee142b
Irene <owlishDeveloper@users.noreply.github.com>
2017-04-15 11:38:11 -0700

    Update insertionSort.js

--- sort/insertionSort.js
+++ sort/insertionSort.js
@@ -1,11 +1,17 @@
 // O(n^2)
 
 function insertionSort(array) {
+  // Sorts an array by inserting each next number
+  // in the right place in the sorted part of the array
+  // to the left of the number.
+  // Sorts in increasing order
   for (var i=1; i<array.length; i++) {
     insert(array, i-1, array[i]);
   }
   
   function insert(array, rightIndex, value) {
+    // Helper function for inserting number (value)
+    // into the sorted part of the array (up to array[rightIndex]).
     for (var j=rightIndex; j>=0 && array[j]>value; j--) {
       array[j+1] = array[j];
     }
